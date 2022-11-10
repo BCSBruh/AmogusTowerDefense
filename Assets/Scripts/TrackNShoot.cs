98aa9da7db143ce9ec1b6922fc69f5dd754ab546
BCSBruh <jeromelarson26@gmail.com>
2022-11-10 17:37:54 -0500

    bruh

--- Assets/Scripts/TrackNShoot.cs
+++ Assets/Scripts/TrackNShoot.cs
@@ -4,26 +4,49 @@ using UnityEngine;
 
 public class TrackNShoot : MonoBehaviour
 {
-    [SerializeField] GameObject pylon;
-    [SerializeField] GameObject radius;
-    [SerializeField] GameObject enemy;
+    [SerializeField] Transform[] enemy = new Transform[10];
 
     [SerializeField] bool inCollider = false;
+
+    [SerializeField] int i = 0;
+    [SerializeField] int j = 0;
     // Start is called before the first frame update
     void Start()
     {
-        pylon = transform.GetChild(0).gameObject.transform.GetChild(1).gameObject;
-        radius = transform.GetChild(1).gameObject;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //pylon.transform.LookAt(enemy);
+        if (inCollider)
+        {
+            transform.LookAt(enemy[0]);
+        }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
-        
+        Debug.Log("Here");
+        inCollider = true;
+        enemy[i] = collision.gameObject.transform;
+        i++;
+        if (j > 0)
+        {
+            j--;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        enemy[j] = null;
+        j++;
+        i--;
+
+        if (i == 0)
+        {
+            inCollider = false;
+        }
     }
 }
